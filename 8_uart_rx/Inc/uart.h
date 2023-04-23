057c648629409d63730382033c7496d28b3e7bce
Bukiezrah <79102300+ezydoez-ezrahogori@users.noreply.github.com>
2023-04-23 10:53:09 +0300

    Uart_rx

---
+++ 8_uart_rx/Inc/uart.h
@@ -0,0 +1,17 @@
+/*
+ * uart.h
+ *
+ *  Created on: Mar 30, 2023
+ *      Author: user
+ */
+
+#ifndef UART_H_
+#define UART_H_
+#include<stdint.h>
+#include<stm32f4xx.h>
+
+void uart2_tx_init(void);
+char uart2_read(void);
+void uart2_rxtx_init(void);
+
+#endif /* UART_H_ */
